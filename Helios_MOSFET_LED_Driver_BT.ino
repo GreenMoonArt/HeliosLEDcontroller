ae347c2a28656b23653b813c8441ca294e91f6cf
GreenMoonArt <greenmoonart@yahoo.com>
2016-04-10 20:35:08 -0400

    Added files via upload

---
+++ Helios_MOSFET_LED_Driver_BT.ino
@@ -0,0 +1,230 @@
+/*********************************************************************
+ Main components: 
+
+ Arduino Pro Mini
+
+ Adafruit Bluefruit LE SPI Friend 
+ https://www.adafruit.com/products/2633
+
+ Bluetooth code: provided by Adafruit 
+ MIT license, check LICENSE for more information
+ ----------------------------------
+
+ Project overview: 
+
+ Underlighting for sculptural art. Diffused white LEDs will be turned on via 
+ Bluetooth, using Adafruit's Bluefruit LE app. 
+ https://itunes.apple.com/us/app/adafruit-bluefruit-le-connect/id830125974
+ using Controller / Color Picker
+ 
+ LEDs will remain on until turned off by the user. 
+ There are two levels of brightness: full and half bright 
+ Using the colorpicker: red = full; green = half; blue = off
+ Use an Arduino PWM pin to regulate brightness
+
+ concept: Nasi Glekas
+ completed: January 2016 by Johnny Dukovich @GreenMoonArt
+ 
+*********************************************************************/
+
+#include <string.h>
+#include <Arduino.h>
+#include <SPI.h>
+#if not defined (_VARIANT_ARDUINO_DUE_X_) && not defined (_VARIANT_ARDUINO_ZERO_)
+  #include <SoftwareSerial.h>
+#endif
+
+#include "Adafruit_BLE.h"
+#include "Adafruit_BluefruitLE_SPI.h"
+#include "Adafruit_BluefruitLE_UART.h"
+
+#include "BluefruitConfig.h"
+
+
+/*=========================================================================
+    APPLICATION SETTINGS
+
+    FACTORYRESET_ENABLE       Perform a factory reset when running this sketch
+   
+                              Enabling this will put your Bluefruit LE module
+                              in a 'known good' state and clear any config
+                              data set in previous sketches or projects, so
+                              running this at least once is a good idea.
+   
+                              When deploying your project, however, you will
+                              want to disable factory reset by setting this
+                              value to 0.  If you are making changes to your
+                              Bluefruit LE device via AT commands, and those
+                              changes aren't persisting across resets, this
+                              is the reason why.  Factory reset will erase
+                              the non-volatile memory where config data is
+                              stored, setting it back to factory default
+                              values.
+       
+                              Some sketches that require you to bond to a
+                              central device (HID mouse, keyboard, etc.)
+                              won't work at all with this feature enabled
+                              since the factory reset will clear all of the
+                              bonding data stored on the chip, meaning the
+                              central device won't be able to reconnect.
+    PIN                       Which pin on the Arduino is connected to the NeoPixels?
+    NUMPIXELS                 How many NeoPixels are attached to the Arduino?
+    -----------------------------------------------------------------------*/
+
+    #define FACTORYRESET_ENABLE     0
+    #define PIN                     5
+
+/*=========================================================================*/
+
+// Create the bluefruit object, either software serial...uncomment these lines
+//SoftwareSerial bluefruitSS = SoftwareSerial(BLUEFRUIT_SWUART_TXD_PIN, BLUEFRUIT_SWUART_RXD_PIN);
+
+//Adafruit_BluefruitLE_UART ble(bluefruitSS, BLUEFRUIT_UART_MODE_PIN,
+//                      BLUEFRUIT_UART_CTS_PIN, BLUEFRUIT_UART_RTS_PIN);
+
+/* ...or hardware serial, which does not need the RTS/CTS pins. Uncomment this line */
+//Adafruit_BluefruitLE_UART ble(BLUEFRUIT_HWSERIAL_NAME, BLUEFRUIT_UART_MODE_PIN);
+
+/* ...hardware SPI, using SCK/MOSI/MISO hardware SPI pins and then user selected CS/IRQ/RST */
+Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_CS, BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);
+
+/* ...software SPI, using SCK/MOSI/MISO user-defined SPI pins and then user selected CS/IRQ/RST */
+//Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_SCK, BLUEFRUIT_SPI_MISO,
+//                             BLUEFRUIT_SPI_MOSI, BLUEFRUIT_SPI_CS,
+//                             BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);
+
+
+// A small helper
+void error(const __FlashStringHelper*err) {
+  Serial.println(err);
+  while (1);
+}
+
+// function prototypes over in packetparser.cpp
+uint8_t readPacket(Adafruit_BLE *ble, uint16_t timeout);
+float parsefloat(uint8_t *buffer);
+void printHex(const uint8_t * data, const uint32_t numBytes);
+
+// the packet buffer
+extern uint8_t packetbuffer[];
+
+long currentTime;
+long loopTime;
+long timeLimit = 1000;   //require 1 second to pass before accepting a new input (aids in "debouncing")
+
+/**************************************************************************/
+/*!
+    @brief  Sets up the HW an the BLE module (this function is called
+            automatically on startup)
+*/
+/**************************************************************************/
+void setup(void)
+{
+  while (!Serial);  // required for Flora & Micro
+  delay(500);
+
+  currentTime = millis();
+  loopTime = currentTime;
+
+  Serial.begin(115200);
+  Serial.println(F("Adafruit Bluefruit Neopixel Color Picker Example"));
+  Serial.println(F("------------------------------------------------"));
+
+  /* Initialise the module */
+  Serial.print(F("Initialising the Bluefruit LE module: "));
+
+  if ( !ble.begin(VERBOSE_MODE) )
+  {
+    error(F("Couldn't find Bluefruit, make sure it's in CoMmanD mode & check wiring?"));
+  }
+  Serial.println( F("OK!") );
+
+  if ( FACTORYRESET_ENABLE )
+  {
+    /* Perform a factory reset to make sure everything is in a known state */
+    Serial.println(F("Performing a factory reset: "));
+    if ( ! ble.factoryReset() ){
+      error(F("Couldn't factory reset"));
+    }
+  }
+
+  /* Disable command echo from Bluefruit */
+  ble.echo(false);
+
+  Serial.println("Requesting Bluefruit info:");
+  /* Print Bluefruit information */
+  ble.info();
+
+  Serial.println(F("Please use Adafruit Bluefruit LE app to connect in Controller mode"));
+  Serial.println(F("Then activate/use the sensors, color picker, game controller, etc!"));
+  Serial.println();
+
+  ble.verbose(false);  // debug info is a little annoying after this point!
+
+  /* Wait for connection */
+  while (! ble.isConnected()) {
+      delay(500);
+  }
+
+  Serial.println(F("***********************"));
+
+  // Set Bluefruit to DATA mode
+  Serial.println( F("Switching to DATA mode!") );
+  ble.setMode(BLUEFRUIT_MODE_DATA);
+
+  Serial.println(F("***********************"));
+
+}
+
+/**************************************************************************/
+/*!
+    @brief  Constantly poll for new command or response data
+*/
+/**************************************************************************/
+void loop(void)
+{
+  /* Wait for new data to arrive */
+  uint8_t len = readPacket(&ble, BLE_READPACKET_TIMEOUT);
+  if (len == 0) return;
+
+  /* Got a packet! */
+  // printHex(packetbuffer, len);
+
+  currentTime = millis();
+  loopTime = millis();
+
+  if (packetbuffer[1] == 'C')   // a color was sent
+  {
+
+    while(currentTime < (loopTime + timeLimit) )
+    {
+      // read in RGB values for color picker:
+      int red = packetbuffer[2];
+      int green = packetbuffer[3];
+      int blue = packetbuffer[4];
+
+      if(red == 255)           // tapping in the red area turns LEDs on full
+        {
+          analogWrite(PIN, 255);  
+        }
+      else if (green == 255)   // tapping in the green area turns LEDs on about 1/2 power
+        {
+          analogWrite(PIN, 100);
+        }
+      else                     // tapping in the blue area turns LEDs off
+        {
+          analogWrite(PIN, 0);
+        }
+
+
+      currentTime = millis();
+      packetbuffer[1] = 'x';   //use dummy value to clear packet buffer
+
+    } // while(currentTime < (loopTime + timeLimit) )
+
+  }  // if (packetbuffer[1] == 'C') 
+
+  delay(250);     //debounce - tweak to achieve desired response
+
+}
+
